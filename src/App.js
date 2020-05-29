c6fd47b051370f6888455d2d487b5678f1c9eee2
Ryan McGuiness <rcm421@lehigh.edu>
2020-05-29 14:13:29 -0400

    added particles

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
 import React from 'react';
+import Particles from 'react-particles-js';
 import './App.css';
-import { Layout, Header, Navigation, Drawer, Content} from 'react-mdl';
+import { Layout, Header, Navigation, Content} from 'react-mdl';
 import Main from './components/main';
 import { Link } from 'react-router-dom';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 250
+    },
+    size: {
+      value: 3
+    }
+  },
+  interactivity: {
+    events: {
+      onhover: {
+        enable: true,
+        mode: 'repulse'
+      }
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
-  
+       <Particles
+          className='particles'
+          params={particlesOptions}
+        />
       <Layout fixedHeader>
         <Header className='header-color'>
           <Navigation className= 'header-tabs'>
                 <Link to="/" className= 'header-tab1'>Home</Link>
-                {/* <Link to="/aboutme">About Me</Link> */}
                 <Link to="/resume" className= 'header-tab2'>Resume</Link>
                 <Link to="/contact" className= 'header-tab3'>Contact</Link>
           </Navigation>
--- src/components/contact.js
+++ src/components/contact.js
@@ -24,11 +24,11 @@ class Contact extends Component {
 						</li>
 						<div className='contact-links'>
 							{/* Linkedin */}
-							<a href="https://www.linkedin.com/in/ryan-mcguiness-733265191/" rel="noopener norefferer" target="_blank">
+							<a href="https://www.linkedin.com/in/ryan-mcguiness-733265191/" rel="noopener norefferer" >
 								<i className="fa fa-linkedin-square" aria-hidden="true"/>
 							</a>
 							{/* Github */}
-							<a href="https://github.com/rymcguin" rel="noopener norefferer" target="_blank">
+							<a href="https://github.com/rymcguin" rel="noopener norefferer" >
 								<i className="fa fa-github-square" aria-hidden="true"/>
 							</a>
 					
